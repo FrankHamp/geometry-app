c513d5821a0a4d9abf09c73e1824b715f6929162
FrankHamp <valdemarengelund@gmail.com>
2026-04-24 15:33:25 +0200

    nr #4

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -1,19 +1,35 @@
+import java.util.Scanner;
+
 class Rectangle implements Shape {
-    private double length;
-    private double width;
+    double length;
+    double width;
 
     public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
 
+    public static void createRectangle(Scanner scanner) {
+        System.out.println("Enter Length:");
+        double length = scanner.nextDouble();
+        double width = scanner.nextDouble();
+
+        Rectangle rectangle = new Rectangle(length, width);
+
+        double area = rectangle.calculateArea();
+        double perimeter = rectangle.calculatePerimeter();
+
+        System.out.println("length: " + length);
+        System.out.println("width: " + width);
+    }
+
     @Override
-    public double getArea() {
+    public double calculateArea() {
         return length * width;
     }
 
     @Override
-    public double getPerimiter() {
+    public double calculatePerimeter() {
         return 2 * (length + width);
     }
 }
