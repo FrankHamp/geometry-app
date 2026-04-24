0f442caa8d51d140674d4dfd45831cea3778852f
FrankHamp <valdemarengelund@gmail.com>
2026-04-24 15:02:18 +0200

    nr #2

---
+++ src/main/java/Rectangle.java
@@ -0,0 +1,19 @@
+class Rectangle implements Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public double getPerimiter() {
+        return 2 * (length + width);
+    }
+}
