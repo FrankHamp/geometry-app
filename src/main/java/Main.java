60a1e5dff1f46dd4f27155a7ddebf768e413a6ae
holmganga <siggi.sejr@gmail.com>
2026-04-27 12:50:03 +0200

    merge update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
-//kode der skal refactors
 public class Main {
-
-    // main skal være mindre
 
     public static void main(String[] args) {
         ShapeCreator creator = new ShapeCreator();
 
         creator.createShape();
     }
+}
